8da35ad481167acbdb4694d135a7aed592e5d729
kleklai <mmagallen@klaiklai.tech>
2022-02-23 21:35:09 +0800

    return value

--- app/Http/Controllers/Api/v1/PersonalInformationController.php
+++ app/Http/Controllers/Api/v1/PersonalInformationController.php
@@ -11,35 +11,31 @@ class PersonalInformationController extends Controller
     public function store(Request $request)
     {
 
-        return response('Maynard Test', 200);
+        $personalInformation = PersonalInformation::create([
+            'id_code'               => $request->idCode,
+            'invite_code'           => $request->inviteCode,
+            'organization'          => $request->organization,
+            'first_name'            => $request->personalInformation['firstName'],
+            'middle_name'           => $request->personalInformation['middleName'],
+            'last_name'             => $request->personalInformation['lastName'],
+            'gender'                => $request->personalInformation['gender'],
+            'civil_status'          => $request->personalInformation['civilStatus'],
+            'birth_date'            => $request->personalInformation['birthDate'],
+            'address_barangay'      => $request->address['barangay'],
+            'address_city'          => $request->address['city'],
+            'address_province'      => $request->address['province'],
+            'address_region'        => $request->address['region'],
+            'address_street'        => $request->address['street'],
+            'address_zip_code'      => $request->address['zipcode'],
+            'email'                 => $request->contactInformation['email'],
+            'contact_number'        => $request->contactInformation['contactNumber'],
+        ]);
 
-        // $personalInformation = PersonalInformation::create([
-        //     'id_code'               => $request->idCode,
-        //     'invite_code'           => $request->inviteCode,
-        //     'organization'          => $request->organization,
-        //     'first_name'            => $request->personalInformation['firstName'],
-        //     'middle_name'           => $request->personalInformation['middleName'],
-        //     'last_name'             => $request->personalInformation['lastName'],
-        //     'gender'                => $request->personalInformation['gender'],
-        //     'civil_status'          => $request->personalInformation['civilStatus'],
-        //     'birth_date'            => $request->personalInformation['birthDate'],
-        //     'address_barangay'      => $request->address['barangay'],
-        //     'address_city'          => $request->address['city'],
-        //     'address_province'      => $request->address['province'],
-        //     'address_region'        => $request->address['region'],
-        //     'address_street'        => $request->address['street'],
-        //     'address_zip_code'      => $request->address['zipcode'],
-        //     'email'                 => $request->contactInformation['email'],
-        //     'contact_number'        => $request->contactInformation['contactNumber'],
-        // ]);
+        $data = [
+            "SUCCESS",
+            $personalInformation,
+        ];
 
-        // $data = [
-        //     "SUCCESS",
-        //     $personalInformation,
-        // ];
-
-        // // return response($request, 200);
-        // return response($data, 200);
-        // return response($request->personalInformation['firstName'], 200);
+        return response($data, 200);
     }
 }
