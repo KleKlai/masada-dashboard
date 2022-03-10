670e2415a7c845b4acd8ac41cda79ddb83730daa
kleklai <mmagallen@klaiklai.tech>
2022-03-10 19:28:23 +0800

    Remove throttle

--- routes/api.php
+++ routes/api.php
@@ -19,4 +19,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('/store', [PersonalInformationController::class, 'store'])->middleware('throttle:1,100');
+Route::post('/store', [PersonalInformationController::class, 'store']);
